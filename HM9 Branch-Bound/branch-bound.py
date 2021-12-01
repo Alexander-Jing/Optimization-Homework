7ead442bdfd262ad0aaaa0eb061975e4c28720e8
Jing <54467153+Alexcander-Jing@users.noreply.github.com>
2021-12-01 15:51:28 +0800

    Add files via upload

---
+++ HM9 Branch-Bound/branch-bound.py
@@ -0,0 +1,19 @@
+from Binary_Tree import BTree
+import numpy as np
+from queue import Queue,LifoQueue,PriorityQueue
+
+def branch_bound():
+    """the branch-and-bound algorithm to solve the problem of HM9
+
+    using the binary tree as the decision making, will form the tree
+    """
+    tree = BTree(0)  # initialize the tree, the left is 0, while the right is 1
+    J_bset = np.inf  # initialize the best value J_best as +infinity 
+    Q_nodes = Queue(maxsize=0)  # initialize the queue 
+    # the data structure of a node: 
+    # k, the kth decision;
+    # u, the decision 0 or 1 
+    # J_pre, the predicted minimum; 
+    # J_result, if the node is the leaf, it is the result output of the node 
+    Q_nodes.put({'k':1,'u':0,'J_pre':0,'J_result':0}) 
+    Q_nodes.put({'k':1,'u':1,'J_pre':0,'J_result':0}) 
